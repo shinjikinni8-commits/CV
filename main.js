c07a4185505f4eb8e7107c84e22a2fdbb9bdca9a
aaa <aaa.gmail.com>
2025-10-27 01:37:42 +0200

    fedvgr

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// JS для прототипу взаємодії
 function toggleSection(id) {
     var element = document.getElementById(id);
     element.classList.toggle('active');
 }
 
-// Smooth scroll (якщо додати посилання, наприклад <a href="#about">)
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
